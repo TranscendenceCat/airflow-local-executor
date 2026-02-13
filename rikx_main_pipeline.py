083109a2720d6f8314142e93b5231d70c5fb0a76
TranscendenceCat <50266748+TranscendenceCat@users.noreply.github.com>
2026-02-14 01:03:05 +0300

    Update rikx_main_pipeline.py

--- rikx_main_pipeline.py
+++ rikx_main_pipeline.py
@@ -7,8 +7,8 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    # 'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
@@ -25,24 +25,7 @@ with DAG(
     user_data_calc = ClickHouseOperator(
         task_id='user_data',
         clickhouse_conn_id='rikx_ch',
-        sql=[
-            """drop table if exists vitrines.user_data""",
-            """create table vitrines.user_data as
-            select
-            	user_id,
-            	min(event_time) as install_time,
-            	if(
-            		(anyIf(toString(parameters.get_request.utm_campaign), event_name = 'registered') as campaign) > '',
-            		campaign,
-            		'other'
-            	) as utm_campaign
-            from rikx.events
-            where app_version != 'dashboards_test'
-              and event_time >= '2026-02-01'
-              and event_time <= '2027-01-01'
-              and app_version >= '0.30.3'
-            group by user_id""",
-        ],
+        sql="""show databases""",
         database='default',
     )
 
